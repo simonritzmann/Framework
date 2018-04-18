f0677f3e099bc171cccaaf09099d6a61bdebb048
Simon Ritzmann <simon.ritzmann@gmail.com>
2018-04-18 22:41:28 +0200

    add FastRoute for routing

--- public/index.php
+++ public/index.php
@@ -1,24 +1,37 @@
 <?php
 declare(strict_types = 1);
 
-require __DIR__ . "/../vendor/autoload.php";
+require __DIR__."/../vendor/autoload.php";
 
+use FastRoute\Dispatcher;
+use FastRoute\RouteCollector;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();
 
-$map    = [
-    '/sample' => "sample"
-];
+$dispatcher = FastRoute\simpleDispatcher(function(RouteCollector $rc) {
+    $routes = include(__DIR__."/../src/routes.php");
+    foreach ($routes as $route) {
+        $rc->addRoute($route[0], $route[1], $route[2]);
+    }
+});
 
-$path = $request->getPathInfo();
-if (isset($map[$path])) {
-    ob_start();
-    include sprintf(__DIR__."/../src/templates/%s.php", $map[$path]);
-    $response = new Response(ob_get_clean());
-} else {
-    $response = new Response("Page not found", 404);
+$routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPathInfo());
+switch ($routeInfo[0]) {
+    case Dispatcher::FOUND:
+        $controller = $routeInfo[1];
+        $response = call_user_func($controller, $request);
+        break;
+    case Dispatcher::NOT_FOUND:
+        $response = new Response("Page not found", 404);
+        break;
+    case Dispatcher::METHOD_NOT_ALLOWED:
+        $response = new Response("Method not allowed", 405);
+        break;
+    default:
+        $response = new Response("Internal server error", 500);
+        break;
 }
 
 $response->send();
---
+++ src/routes.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types = 1);
+$routes = [];
+
+$routes[] = ["GET", "/sample", ""];
+
+return $routes;
