2fdeccf48cdc7a546e3a29a48807833e4ba264de
Simon Ritzmann <simon.ritzmann@gmail.com>
2018-04-18 17:09:41 +0200

    refactor front controller

--- public/index.php
+++ public/index.php
@@ -1,27 +1,24 @@
 <?php
 declare(strict_types = 1);
 
-require __DIR__ . "/../src/bootstrap.php";
 require __DIR__ . "/../vendor/autoload.php";
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();
-$response = new Response();
 
 $map    = [
-    '/sample' => __DIR__ . "/../src/templates/sample.php"
+    '/sample' => "sample"
 ];
 
 $path = $request->getPathInfo();
 if (isset($map[$path])) {
     ob_start();
-    include $map[$path];
-    $response->setContent(ob_get_clean());
+    include sprintf(__DIR__."/../src/templates/%s.php", $map[$path]);
+    $response = new Response(ob_get_clean());
 } else {
-    $response->setStatusCode(404);
-    $response->setContent("Page not found");
+    $response = new Response("Page not found", 404);
 }
 
 $response->send();
